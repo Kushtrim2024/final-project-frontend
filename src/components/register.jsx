1e254e9bb15490154336f350283d934e60b3c6a1
Kushtrim Bilali <kushtro20@gmail.com>
2025-07-17 12:07:43 +0200

    initial commit

---
+++ src/components/register.jsx
@@ -0,0 +1,21 @@
+export const Register = () => {
+
+   
+
+  return (
+    <div className="register-container">
+      <h2>Register</h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
