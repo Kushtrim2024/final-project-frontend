2560cd135fad5566452ee9aec0bafc3e7d04df59
CihanUnall <cihanunal_20@hotmail.com>
2025-08-20 11:51:50 +0200

    fix merge chn

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-import Link from "next/link";
-
-export default function AdminLayout({ children }) {
-  return (
-    <div className="min-h-screen flex">
-      {/* Sidebar */}
-      <aside className="w-64 bg-gray-900 text-white p-6 space-y-4">
-        <h1 className="text-xl font-bold mb-6">Admin Management</h1>
-        <nav className="space-y-2">
-          <a href="/admin" className="block hover:text-gray-300">
-            Dashboard
-          </a>
-          <a href="/admin/restaurants" className="block hover:text-gray-300">
-            Restaurants
-          </a>
-          <a href="/admin/roles" className="block hover:text-gray-300">
-            Roles
-          </a>
-          <a href="/admin/permissions" className="block hover:text-gray-300">
-            Permissions
-          </a>
-        </nav>
-        <div>
-          <Link href="/">
-            <img src="/logo.png" alt="Logo" className="w-20 h-20 mb-2" />
-          </Link>
-        </div>
-      </aside>
-
-      <main className="flex-1 p-8 bg-gray-100">{children}</main>
-    </div>
-  );
-}
-=======
 // AdminLayout.jsx
 import React from "react";
 import NavAdmin from "./componentsadmin/NavAdmin.jsx";
@@ -69,4 +34,3 @@ function AdminLayout({ children }) {
   );
 }
 export default AdminLayout;
->>>>>>> chnbranch
